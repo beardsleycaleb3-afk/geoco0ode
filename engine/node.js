8027a8b2bea350b730b0fbbf6decab37f900a6e0
beardsleycaleb3-afk <beardsleycaleb3@gmail.com>
2026-04-06 17:27:59 -0400

    Update node.js

--- engine/node.js
+++ engine/node.js
@@ -1,7 +1,3 @@
-/**
- * GEO-CO0ODE NODE
- * The individual data unit for 018810 mapping.
- */
 const Node = {
     create: function(id, value = 0) {
         return {
@@ -12,11 +8,9 @@ const Node = {
             isPivot: id === 88 || id % 100 === 88
         };
     },
-    
     // Physical Bit Reversal for 255-unit matrix compatibility
     reverse: function(byte) {
         return parseInt(byte.toString(2).padStart(8, '0').split('').reverse().join(''), 2);
     }
 };
-
 export default Node;
