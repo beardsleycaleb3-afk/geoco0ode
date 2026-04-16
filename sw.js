4f7ca4388f7a328d58d83a5356784c73fb4257f3
beardsleycaleb3-afk <beardsleycaleb3@gmail.com>
2026-04-16 08:27:45 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'geocoode';
+const ASSETS = [
+  './',
+  './index.html',
+  './icon.png',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate & Cleanup Old Caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Strategy: Cache First, then Network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
